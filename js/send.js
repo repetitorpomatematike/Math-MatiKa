5844cff5f361e843d3a52274d7b4dae611cfc52c
Mikhail <mikatagarov@gmail.com>
2023-08-17 22:12:00 +0300

    update

--- js/send.js
+++ js/send.js
@@ -3,7 +3,6 @@ function sendForm(form) {
   const inputs = form.querySelectorAll('input')
   const preloader = document.querySelector('.preloader');
   const modals = document.querySelectorAll(".modal");
-
   $.ajax({
       url: "https://smartswim.ru/ajax/handler.php",
       type: "POST",
@@ -14,9 +13,15 @@ function sendForm(form) {
       beforeSend: () => {
         console.log("Отправлено");
           preloader.classList.add("shown")
-          inputs.forEach((input) => {
-            input.disabled = true
-          })
+          inputs.forEach(input => input.disabled = true);
+      },
+      success: function(response) {
+        console.log(response);
+        
+      },
+      error: function(error) {
+        console.table(error);
+        inputs.forEach((input) => input.disabled = false);
           setTimeout(() => {
             preloader.classList.remove("shown");
             openModal(document.querySelector(".thank-modal"));
